93f63130f4ff42cfa88d044183d8a41e6aa0d2af
SEUNGHEE LEE <lee2002w@gmail.com>
2024-02-27 00:57:47 +0900

    GPIO isr handler - queue + task

--- main/include/peripheral/pmodpirctrl.h
+++ main/include/peripheral/pmodpirctrl.h
@@ -24,14 +24,18 @@ public:
 public:
     bool initialize(uint8_t gpio_num);
     void register_callback_gpio_change(fn_gpio_change_callback callback);
-    uint8_t get_gpio_num();
     void set_callback(uint8_t level);
+    QueueHandle_t get_queue_isr();
 
 private:
     static CPmodPIRCtrl* _instance;
-
+    bool m_keepalive;
     uint8_t m_gpio_num;
     fn_gpio_change_callback m_callback_ptr;
+    QueueHandle_t m_queue_isr;
+    TaskHandle_t m_task_handle_isr;
+
+    static void task_gpio_isr_function(void *param);
 };
 
 inline CPmodPIRCtrl* GetPmodPIRCtrl() {
--- main/src/peripheral/pmodpirctrl.cpp
+++ main/src/peripheral/pmodpirctrl.cpp
@@ -1,26 +1,32 @@
 #include "pmodpirctrl.h"
 #include "logger.h"
 #include "driver/gpio.h"
+#include "definition.h"
 
 CPmodPIRCtrl* CPmodPIRCtrl::_instance = nullptr;
 
 static void IRAM_ATTR gpio_isr_handler(void* arg)
 {
     CPmodPIRCtrl *obj = reinterpret_cast<CPmodPIRCtrl*>(arg);
-    uint8_t gpio_num = obj->get_gpio_num();
-    uint8_t level = (uint8_t)gpio_get_level((gpio_num_t)gpio_num);
-    obj->set_callback(level);
+    QueueHandle_t queue = obj->get_queue_isr();
+    uint8_t temp = 0;
+    xQueueSendFromISR(queue, &temp, nullptr);
 }
 
 CPmodPIRCtrl::CPmodPIRCtrl()
 {
     m_gpio_num = 0;
     m_callback_ptr = nullptr;
+    m_queue_isr = xQueueCreate(10, sizeof(uint8_t));
+    m_keepalive = true;
+    xTaskCreate(task_gpio_isr_function, "PMOD_PIR_TASK", TASK_STACK_DEPTH, this, 10, &m_task_handle_isr);
 }
 
 CPmodPIRCtrl::~CPmodPIRCtrl()
 {
+    m_keepalive = false;
     gpio_isr_handler_remove((gpio_num_t)m_gpio_num);
+    m_callback_ptr = nullptr;
 }
 
 CPmodPIRCtrl* CPmodPIRCtrl::Instance()
@@ -40,7 +46,7 @@ bool CPmodPIRCtrl::initialize(uint8_t gpio_num)
     gpio_config_t cfg = {};
     cfg.pin_bit_mask = 1ULL << m_gpio_num;
     cfg.mode = GPIO_MODE_INPUT;
-    cfg.pull_up_en = GPIO_PULLUP_DISABLE;
+    cfg.pull_up_en = GPIO_PULLUP_ENABLE;
     cfg.pull_down_en = GPIO_PULLDOWN_DISABLE;
     cfg.intr_type = GPIO_INTR_ANYEDGE;  // detect both occupancy and no occupancy
     ret = gpio_config(&cfg);
@@ -69,9 +75,9 @@ void CPmodPIRCtrl::register_callback_gpio_change(fn_gpio_change_callback callbac
     m_callback_ptr = std::move(callback);
 }
 
-uint8_t CPmodPIRCtrl::get_gpio_num()
+QueueHandle_t CPmodPIRCtrl::get_queue_isr()
 {
-    return m_gpio_num;
+    return m_queue_isr;
 }
 
 void CPmodPIRCtrl::set_callback(uint8_t level)
@@ -79,4 +85,19 @@ void CPmodPIRCtrl::set_callback(uint8_t level)
     if (m_callback_ptr) {
         m_callback_ptr(level);
     }
+}
+
+void CPmodPIRCtrl::task_gpio_isr_function(void *param)
+{
+    CPmodPIRCtrl *obj = reinterpret_cast<CPmodPIRCtrl*>(param);
+    uint8_t temp = 0;
+
+    while (obj->m_keepalive) {
+        if (xQueueReceive(obj->m_queue_isr, (void *)&temp, 1) == pdTRUE) {
+            uint8_t level = (uint8_t)gpio_get_level((gpio_num_t)obj->m_gpio_num);
+            GetLogger(eLogType::Info)->Log("GPIO Level changed to %u", level);
+            obj->set_callback(level);
+        }
+    }
+    vTaskDelete(nullptr);
 }
